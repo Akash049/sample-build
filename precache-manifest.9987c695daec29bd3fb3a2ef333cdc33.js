0eb8c3926b1b3d8f90271fce1320d1ad2341842c
MelpApp ML <melpml@gmail.com>
2020-02-12 14:43:13 +0530

    setup

---
+++ precache-manifest.9987c695daec29bd3fb3a2ef333cdc33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfa128b98a7a97ffa578",
+    "url": "/static/css/main.ea243b1a.chunk.css"
+  },
+  {
+    "revision": "cfa128b98a7a97ffa578",
+    "url": "/static/js/main.63888777.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1d12119a83612ce29ed6",
+    "url": "/static/css/2.db2fa2fc.chunk.css"
+  },
+  {
+    "revision": "1d12119a83612ce29ed6",
+    "url": "/static/js/2.bd2c2657.chunk.js"
+  },
+  {
+    "revision": "d7118ed1560740c0667e0311288482b3",
+    "url": "/index.html"
+  }
+];
